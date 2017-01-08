70b3c0f2f4f6a0445df5ca082fde43768645ffa9
Lifecoder <matthias.hoste2@protonmail.com>
2017-01-08 03:57:11 +0100

    Started work on I2P base

---
+++ SafeShare/Core/Networking/Proxy/I2P/I2PProxyServer.cs
@@ -0,0 +1,23 @@
+﻿using FuhrerShare.Core.Networking.Clients;
+using net.i2p.client.streaming;
+using net.i2p.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuhrerShare.Core.Networking.Proxy.I2P
+{
+    internal class I2PProxyServer
+    {
+        internal void CreateI2PConnection2Node(SafeNode node, out SslStream ssl)
+        {
+            I2PSocketManager manager = I2PSocketManagerFactory.createManager();
+            Destination D = new Destination(node.hiddenid);
+            I2PSocket socket = manager.connect(D);
+            ssl = null;
+        }
+    }
+}
